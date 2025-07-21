f889063f2c44670035abcc5a6a3ccd81062067c2
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-07-21 10:15:32 +0200

    Refactor Firebase initialization and add placeholder files for login, signup, upload image, and text field screens

--- holbegram/lib/firebase_options.dart
+++ holbegram/lib/firebase_options.dart
@@ -65,4 +65,5 @@ class DefaultFirebaseOptions {
     storageBucket: 'holbegram-a5645.firebasestorage.app',
     iosBundleId: 'com.example.holbegram',
   );
+
 }
--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; // Ce fichier sera généré automatiquement par `flutterfire configure`
 
-void main() async {
-  // Ensure Flutter bindings are initialized before calling native code
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
